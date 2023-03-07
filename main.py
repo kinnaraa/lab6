7f66b9fb29251543c7e8c68122e555bb5cc6b952
kinnaraa <kinnarabosworth@gmail.com>
2023-03-07 12:16:51 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,47 @@
+# Kinnara Bosworth
+def print_menu():
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+
+
+def encoder(string):
+    encoded = []
+    for char in string:
+        encoded.append(int(char)+3)
+    new_encoded = ""
+    for num in encoded:
+        new_encoded += str(num)
+    return new_encoded
+
+
+def decoder(string):
+    decoded = []
+    for char in string:
+        decoded.append(int(char)-3)
+    new_decoded = ""
+    for num in decoded:
+        new_decoded += str(num)
+    return new_decoded
+
+
+def main():
+    option = ""
+    while option != 3:
+        print_menu()
+        print()
+        option = int(input("Please enter an option: "))
+        if option == 1:
+            string = input("Please enter your password to encode: ")
+            print("Your password has been encoded and stored!")
+        elif option == 2:
+            print(f"The encoded password is {encoder(string)}, and the original password is {string}.")
+        elif option == 3:
+            break
+        print()
+
+
+if __name__ == "__main__":
+    main()
